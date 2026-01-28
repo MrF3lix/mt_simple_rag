0f84b3d9e9941786ce2bd395ee772c2fb605af12
Felix Saaro <saaf@zhaw.ch>
2026-01-28 13:21:12 +0100

    Update wikipedia index ivf

--- src/script/wikipedia_index_ivf.py
+++ src/script/wikipedia_index_ivf.py
@@ -5,7 +5,9 @@ import faiss
 
 INDEX_FILE = "data/test_ivf.index"
 TARGET_DB = 'data/all.duckdb'
+TEMP_TRAINING_EMBEDDINGS = 'data/training_embeddings.npx'
 DIM = 1024
+QUERY_BATCH_SIZE = 100_000
 BATCH_SIZE = 1000
 NLIST = 65536
 NPROBE = 16
@@ -28,16 +30,39 @@ index = faiss.IndexIVFFlat(
     faiss.METRIC_INNER_PRODUCT
 )
 
-train_batch = con.execute("""
+print('Prepare Query')
+cursor = con.execute("""
     SELECT vec FROM paragraph
     WHERE has_vec = TRUE
-    LIMIT ?
-""", [TRAINING_SIZE]).fetchall()
+""")
 
-train_embeddings = np.asarray([x[0] for x in train_batch], dtype="float32")
+
+pbar = tqdm(total=TRAINING_SIZE)
+print('Start Fetching')
+train_embeddings = []
+processed = 0 
+while True:
+    rows = cursor.fetchmany(QUERY_BATCH_SIZE)
+    if not rows:
+        break
+
+    embeddings = [x[0] for x in rows]
+    embeddings = np.asarray(embeddings, dtype="float32")
+
+    train_embeddings.extend(embeddings)
+    pbar.update(len(embeddings))
+
+    processed += len(embeddings)
+    if processed > TRAINING_SIZE:
+        break
+
+print('Compress and Save')
+train_embeddings = np.asarray(train_embeddings, dtype="float32")
+np.savez_compressed(TEMP_TRAINING_EMBEDDINGS, train_embeddings)
+
+print('Saved Training Embeddings')
+
 index.train(train_embeddings)
-
-index.reserve(total)
 
 pbar = tqdm(total=total)
 offset = 0

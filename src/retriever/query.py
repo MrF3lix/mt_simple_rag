d09185f0749411c008fa90fe2a93fc3d3cecb97c
Felix Saaro <saaf@zhaw.ch>
2026-01-28 13:20:29 +0100

    Update query results

--- src/retriever/query.py
+++ src/retriever/query.py
@@ -14,7 +14,8 @@ class Query(BaseModel):
     answer: Optional[str] = None
     generated_answer: Optional[str] = None
     use_llm_judge: bool = False
-    is_answer_correct: Optional[bool] = None
+    llm_judge_answer: Optional[str] = False
+    is_answer_correct: bool = None
     retrieved_correct_document: Optional[bool] = None
     retrieved_correct_paragraph: Optional[bool] = None
     references: list[Paragraph] = []
@@ -28,6 +29,8 @@ class Query(BaseModel):
             'retrieved': list(map(lambda r: r.model_dump(), self.retrieved)),
             'answer': self.answer,
             'generated_answer': self.generated_answer,
+            'use_llm_judge': self.use_llm_judge,
+            'llm_judge_answer': self.llm_judge_answer,
             'correct_document': self.retrieved_correct_document,
             'correct_paragraph': self.retrieved_correct_paragraph,
             'correct_answer': self.is_answer_correct,
